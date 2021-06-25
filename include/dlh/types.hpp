57dd63b5a3303f66471716cc83bf1566528e0bf8
Bernhard Heinloth <heinloth@cs.fau.de>
2021-06-25 15:15:48 +0200

    use long instead of long long for int64_t (for better conversion

--- include/dlh/types.hpp
+++ include/dlh/types.hpp
@@ -8,16 +8,16 @@
 typedef unsigned char      uint8_t;
 typedef unsigned short     uint16_t;
 typedef unsigned int       uint32_t;
-typedef unsigned long long uint64_t;
-typedef unsigned long long uintptr_t;
+typedef unsigned long      uint64_t;
+typedef unsigned long      uintptr_t;
 
 typedef __SIZE_TYPE__      size_t;
 
 typedef char               int8_t;
 typedef short              int16_t;
 typedef int                int32_t;
-typedef long long          int64_t;
-typedef long long          intptr_t;
+typedef long               int64_t;
+typedef long               intptr_t;
 
 typedef long int           ssize_t;
 
